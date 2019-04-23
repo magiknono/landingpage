57413f9ce5e0b06889d473b815bb8ad82cf33788
arnaud cormier <arno.cormier@gmail.com>
2019-04-23 13:18:33 +0200

    create the plug and configure the supervisor for cowboy

--- lib/landingpage/application.ex
+++ lib/landingpage/application.ex
@@ -4,17 +4,20 @@ defmodule Landingpage.Application do
   @moduledoc false
 
   use Application
+  require Logger
 
   def start(_type, _args) do
     # List all child processes to be supervised
     children = [
       # Starts a worker by calling: Landingpage.Worker.start_link(arg)
       # {Landingpage.Worker, arg}
+      {Plug.Cowboy, scheme: :http, plug: Landingpage.HomePlug, options: [port: 4000]}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Landingpage.Supervisor]
+    Logger.info("Starting application...")
     Supervisor.start_link(children, opts)
   end
 end
